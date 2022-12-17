9671777139c2dee1980d7f16e309bc23b39e8472
Luis Flores <98134740+lmflores@users.noreply.github.com>
2022-12-17 15:29:17 -0600

    Crear Cuenta e Iniciar Sesión Corregido

--- main.py
+++ main.py
@@ -3,30 +3,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert
 # Avoid site blocking by cloudfire
 import undetected_chromedriver  as uc
+
 driver = uc.Chrome()
-  ###  REGISTRO ##
+
 driver.get("https://ddtech.mx/")
 driver.maximize_window()
-time.sleep(10)
-driver.find_element(By.XPATH, '/html/body/header/div[1]/div/div/div[3]/ul/li[3]/a/font/font').click()
+time.sleep(12)
 
-time.sleep(1)
-
+###  REGISTRO ##
+driver.find_element(By.XPATH, '/html/body/header/div[1]/div/div/div[3]/ul/li[3]/a').click()
+time.sleep(5)
 usuario = driver.find_element(By.ID, "username")
-time.sleep(1)
-correo = driver.find_element(By.ID, "email")
-time.sleep(1)
-contraseña = driver.find_element(By.ID, "password")
-time.sleep(1)
-confir_contra = driver.find_element(By.ID, "confirm_pas")
-time.sleep(1)
-
 usuario.send_keys("Fumito10")
-time.sleep(1)
+correo = driver.find_element(By.ID, "email")
 correo.send_keys("manulop900@gmail.com")
-time.sleep(1)
+contraseña = driver.find_element(By.ID, "password")
 contraseña.send_keys("Fumito-10")
-time.sleep(1)
+confir_contra = driver.find_element(By.ID, "confirm_password")
 confir_contra.send_keys("Fumito-10")
-time.sleep(1)
-driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[2]/form/button').click()
+
+time.sleep(5)
+driver.find_element(By.NAME, 'sign_up').click()
+time.sleep(5)
+
+## INGRESAR CUENTA ##
+
+correo = driver.find_element(By.ID, "email_login")
+correo.send_keys("manulop900@gmail.com")
+contraseña = driver.find_element(By.ID, "password_login")
+contraseña.send_keys("Fumito-10")
+
+time.sleep(5)
+driver.find_element(By.NAME, 'login').click()
+time.sleep(20)
