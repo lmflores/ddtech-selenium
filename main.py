6900e8c5cf01323a856f85d750a9ed3435b11dbe
Fumito10 <98134809+Fumito10@users.noreply.github.com>
2022-12-18 13:55:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -35,4 +35,39 @@ contraseña.send_keys("Fumito-10")
 
 time.sleep(5)
 driver.find_element(By.NAME, 'login').click()
+time.sleep(5)
+
+
+## DATOS ENVIO ##
+driver.find_element(By.XPATH, '/html/body/header/div[1]/div/div/div[3]/ul/li[1]/a').click()
+time.sleep(5)
+calle = driver.find_element(By.ID, "form_street")
+numero = driver.find_element(By.ID, "form_number")
+cp = driver.find_element(By.ID, "form_zipcode")
+colonia = driver.find_element(By.ID, "form_colony")
+ciudad = driver.find_element(By.ID, "form_town")
+tel = driver.find_element(By.ID, "form_phone")
+
+time.sleep(5)
+
+calle.send_keys("Venustiano Carranza")
+time.sleep(1)
+numero.send_keys("8")
+time.sleep(1)
+cp.send_keys("42760")
+time.sleep(1)
+colonia.send_keys("Tezontepec de Aldama")
+time.sleep(1)
+ciudad.send_keys("Tezontepec de Aldama")
+time.sleep(1)
+tel.send_keys("1123456789")
+time.sleep(1)
+driver.find_element(By.ID, "form_state").click()
+time.sleep(1)
+driver.find_element(By.XPATH, '//*[@id="form_state"]/option[14]').click()
+
+time.sleep(5)
+driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div[1]/form/div[11]/div/button').click()
+
+
 time.sleep(20)
